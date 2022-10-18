9f4a637a3105466e5d5d3476a0f54e96d2d04fe7
German Fica <germafica@gmail.com>
2022-10-18 13:12:23 -0300

    Add cancellation token source

--- Assets/Scripts/DeviceManager.cs
+++ Assets/Scripts/DeviceManager.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 
 public class DeviceManager : MonoBehaviour
 {
+    private CancellationTokenSource cancellationTokenSource;
+
     void Start()
     {
         Debug.Log("DeviceManager Init");
-        ShowDevices();
+        cancellationTokenSource = new CancellationTokenSource();
+        var cancellationToken = cancellationTokenSource.Token;
+        ShowDevices(cancellationToken);
     }
 
-    private async void ShowDevices()
+    private async void ShowDevices(CancellationToken cancellationToken)
     {
         while (true)
         {
@@ -20,7 +25,12 @@ public class DeviceManager : MonoBehaviour
 
             for (int i = 0; i < devices.Length; i++)
                 Debug.Log(devices[i].name);
-            await Task.Delay(1000);
+            await Task.Delay(1000, cancellationToken);
         }
+    }
+
+    private void OnDisable()
+    {
+        cancellationTokenSource.Cancel(); // Doing this will cancel any currently running task that passed in the CancellationToken reference.
     }
 }
